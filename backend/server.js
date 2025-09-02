ea6a632acbac3238de0fbe23013e22aa9321e917
pragya9111 <pragyabisen14@gmail.com>
2025-09-02 13:39:30 +0530

    remove heroku config

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: 'https://comcares-technologies.vercel.app/'
+}));
 app.use(express.json());
 
 // Routes
